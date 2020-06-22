ddf4a4a9c11494cb102c23cb11ec50d7a73e7330
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-06-22 08:25:04 -0400

    Se agregar los ejercicios 3, 4 y 5 del cap 10 en la clase Poligono en la carpeta Entidades

--- Entidades/Poligono.cs
+++ Entidades/Poligono.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Cap10_12.Entidades
 {
+    //3- Crear una clase para polígonos con sobrecarga del constructor.
     public class Poligono
     {
-        public float Radio = 0;
+        private static float Radio = 0;
         public float Lado = 0;
         public float Apotema = 0;
         public float Angulo = 0;
@@ -16,12 +18,13 @@ namespace Cap10_12.Entidades
 
         public Poligono(float radio, float lado, float apotema, float angulo)
         {
-            Radio = radio;
-            Lado = lado;
-            Apotema = apotema;
-            Angulo = angulo;
+            Radio_ = radio;
+            Lado_ = lado;
+            Apotema_ = apotema;
+            Angulo_ = angulo;
         }
 
+        //Constructor Sobrecargado.
         public Poligono(float radio, float lado, float apotema, float angulo, float inscrita, float circunscrita)
         {
             Radio = radio;
@@ -32,29 +35,115 @@ namespace Cap10_12.Entidades
             Circunscrita = circunscrita;
         }
 
-        public float Radio
+
+        //4- Crear propiedades para la clase polígono que solamente permitan colocar valores válidos.
+        public float Radio_
         {
             get
             {
-                return radio;
+                return Radio;
             }
             set
             {
-                string var = 
+                string var = Radio.ToString();
+                if (!Regex.IsMatch(var, "^[0-9]+$"))
+                    Radio = 0;
+                Radio = value;
+            }
+        }
+
+        public float Lado_
+        {
+            get
+            {
+                return Lado;
+            }
+            set
+            {
+                string var = Lado.ToString();
+                if (!Regex.IsMatch(var, "^[0-9]+$"))
+                    Lado = 0;
+                Lado = value;
+            }
+        }
+
+        public float Apotema_
+        {
+            get
+            {
+                return Apotema;
+            }
+            set
+            {
+                string var = Apotema.ToString();
+                if (!Regex.IsMatch(var, "^[0-9]+$"))
+                    Apotema = 0;
+                Apotema = value;
+            }
+        }
+
+        public float Angulo_
+        {
+            get
+            {
+                return Angulo;
+            }
+            set
+            {
+                string var = Angulo.ToString();
+                if (!Regex.IsMatch(var, "^[0-9]+$"))
+                    Angulo = 0;
+                Angulo = value;
+            }
+        }
+
+        public float Inscrita_
+        {
+            get
+            {
+                return Inscrita;
+            }
+            set
+            {
+                string var = Inscrita.ToString();
+                if (!Regex.IsMatch(var, "^[0-9]+$"))
+                    Inscrita = 0;
+                Inscrita = value;
+            }
+        }
+
+        public float Circunscrita_
+        {
+            get
+            {
+                return Circunscrita;
+            }
+            set
+            {
+                string var = Circunscrita.ToString();
+                if (!Regex.IsMatch(var, "^[0-9]+$"))
+                    Circunscrita = 0;
+                Circunscrita = value;
             }
         }
 
 
+        // 5- Programar el método ToString() para la clase polígono.
 
-        public override string ToString()
-        {
-            StringBuilder stringBuilder = new StringBuilder();
+         public override string ToString()
+         {
+             StringBuilder stringBuilder = new StringBuilder();
 
-            stringBuilder.AppendFormat($"Radio: {radio}");
+            stringBuilder.AppendFormat($"Radio: {Radio}");
+            stringBuilder.AppendFormat($"Lado: {Lado}");
+            stringBuilder.AppendFormat($"Apotema: {Apotema}");
+            stringBuilder.AppendFormat($"Angulo: {Angulo}");
+            stringBuilder.AppendFormat($"Inscrita: {Inscrita}");
+            stringBuilder.AppendFormat($"Circunscrita: {Circunscrita}");
 
             return base.ToString();
 
-        }
+         }
 
     }
 
