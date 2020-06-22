4e840caf6b453aa804cc2bd4fa65f320f2ce8442
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-06-21 20:31:52 -0400

    Se creo la clase Tienda

---
+++ Entidades/Tienda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap10_12.Entidades
+{
+    public class Tienda
+    {
+        public int ArticuloId { get; set; }
+        public string NombreArticulo { get; set; }
+        public float PrecioVenta { get; set; }
+        public float PrecioCompra { get; set; }
+        public int CantidadArticulo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
