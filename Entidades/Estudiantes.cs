b8ce44ef2f738cf79a34dcd0b2265b3d201ff437
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-06-21 20:37:10 -0400

    Se creo la clase Estudiante  en la Carpeta Entidades

---
+++ Entidades/Estudiantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap10_12.Entidades
+{
+    public class Estudiantes
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public string Curso { get; set; }
+        public int EstudianteId { get; set; }
+    }
+}
