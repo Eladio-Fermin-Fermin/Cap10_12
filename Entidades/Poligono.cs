dab3ff977fda85c550f0c5cf27264f54e767e7fc
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-06-21 20:54:26 -0400

    Se creo la clase Poligono en la carpeta Entidades

---
+++ Entidades/Poligono.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cap10_12.Entidades
+{
+    public class Poligono
+    {
+        public float Radio = 0;
+        public float Lado = 0;
+        public float Apotema = 0;
+        public float Angulo = 0;
+        public float Inscrita = 0;
+        public float Circunscrita = 0;
+
+
+        public Poligono(float radio, float lado, float apotema, float angulo)
+        {
+            Radio = radio;
+            Lado = lado;
+            Apotema = apotema;
+            Angulo = angulo;
+        }
+
+        public Poligono(float radio, float lado, float apotema, float angulo, float inscrita, float circunscrita)
+        {
+            Radio = radio;
+            Lado = lado;
+            Apotema = apotema;
+            Angulo = angulo;
+            Inscrita = inscrita;
+            Circunscrita = circunscrita;
+        }
+
+        public float Radio
+        {
+            get
+            {
+                return radio;
+            }
+            set
+            {
+                string var = 
+            }
+        }
+
+
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendFormat($"Radio: {radio}");
+
+            return base.ToString();
+
+        }
+
+    }
+
+}
